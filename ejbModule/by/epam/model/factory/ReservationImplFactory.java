a0ad4b605b9a033e3cea5f6d6e3ea0da3e70cb0e
Anton Piatrouski <piatrouski2a@gmail.com>
2016-07-03 23:22:59 +0300

    returns implementation object using dependency injection

--- ejbModule/by/epam/model/factory/ReservationImplFactory.java
+++ ejbModule/by/epam/model/factory/ReservationImplFactory.java
@@ -1,10 +1,27 @@
 package by.epam.model.factory;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import by.epam.exception.custom.DaoException;
 import by.epam.model.iface.ReservationDAO;
-import by.epam.model.impl.ReservationImplXml;
 
 public class ReservationImplFactory {
-	public static ReservationDAO getImplementation() {
-		return new ReservationImplXml();
+	public static ReservationDAO getImplementation() throws DaoException {
+		try {
+			InitialContext ic = new InitialContext();
+			String implClassName = (String) ic.lookup("java:comp/env/implClass");
+			
+			Class<?> implClass = Class.forName(implClassName);
+			ReservationDAO reservDao = (ReservationDAO) implClass.newInstance();
+			
+			return reservDao;
+			
+		} catch (NamingException | ClassNotFoundException ex) {
+			throw new DaoException("ReservationDAO implementation class is not found", ex);
+			
+		} catch (IllegalAccessException | InstantiationException ex) {
+			throw new DaoException("ReservationDAO implementation creating error", ex);
+		}
 	}
 }
